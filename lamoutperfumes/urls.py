6c55af919b4d7eb7b9c986e569f02e1729f9b114
Lamout Perfume <lamoutperfume@gmail.com>
2025-09-03 21:31:46 +0300

    feat: Add main Django project settings
    - Core Django configuration and URL routing
    - Settings for production and development
    - WSGI configuration for deployment

---
+++ lamoutperfumes/urls.py
@@ -0,0 +1,33 @@
+"""eau_royal URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path("", include("home.urls")),
+    path("catalog/", include(("catalog.urls", "catalog"), namespace="catalog")),
+    path("admin/", admin.site.urls),  # Keep Django admin as fallback
+    path("custom-admin/", include("custom_admin.urls", namespace="custom_admin")),
+    path("accounts/", include("allauth.urls")),  # Add Allauth URLs
+    # Redirect root admin to custom admin
+    path("admin/", RedirectView.as_view(url="/custom-admin/", permanent=False)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = "home.views.error_404"
+handler500 = "home.views.error_500"
